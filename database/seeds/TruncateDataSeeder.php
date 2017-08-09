4e04108b160322073afc506b066cb27c3f509b5b
KuniyasuWada <elephunk.com@i.softbank.jp>
2017-08-09 23:30:36 +0900

    商品関連テーブルのTruncateシーディング作成

--- database/seeds/TruncateDataSeeder.php
+++ database/seeds/TruncateDataSeeder.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Models\Eccube3\DtbClassCategory as E3_DtbClassCategory;
+use App\Models\Eccube3\DtbClassName as E3_DtbClassName;
+
+use App\Models\Eccube3\DtbProduct as E3_DtbProduct;
+use App\Models\Eccube3\DtbProductCategory as E3_DtbProductCategory;
+use App\Models\Eccube3\DtbProductClass as E3_DtbProductClass;
+use App\Models\Eccube3\DtbProductStock as E3_DtbProductStock;
+
+use App\Models\Eccube3\DtbShipmentItem as E3_DtbShipmentItem;
+
 use Illuminate\Database\Seeder;
 
 class TruncateDataSeeder extends Seeder
@@ -11,15 +21,20 @@ class TruncateDataSeeder extends Seeder
      */
     public function run()
     {
-//         Schema::disableForeignKeyConstraints();
+        $Schema = Schema::connection('mysql_eccube3');
+        $Schema->disableForeignKeyConstraints();
 
-//         DB::table('programs')->truncate();
-//         DB::table('episodes')->truncate();
-//         DB::table('tx_videos')->truncate();
-//         DB::table('gyao_videos')->truncate();
-//         DB::table('nico_videos')->truncate();
+        E3_DtbProductStock::truncate();
+        E3_DtbProductCategory::truncate();
+        E3_DtbShipmentItem::truncate();
 
-//         Schema::enableForeignKeyConstraints();
+        E3_DtbProductClass::truncate();
+        E3_DtbProduct::truncate();
+
+        E3_DtbClassCategory::truncate();
+        E3_DtbClassName::truncate();
+
+        $Schema->enableForeignKeyConstraints();
     }
 
 }
