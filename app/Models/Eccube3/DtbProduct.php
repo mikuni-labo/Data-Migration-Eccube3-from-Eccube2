db9aaa88aff8fc81d4f94b7e09cbdf0a55e24a8b
Kuniyasu Wada <mikunilabo@gmail.com>
2017-09-20 15:30:05 +0900

    商品に紐付く商品規格の中で1件でも削除されていないデータがある場合は、属する商品を公開とするロジックをテスト

--- app/Models/Eccube3/DtbProduct.php
+++ app/Models/Eccube3/DtbProduct.php
@@ -3,6 +3,7 @@
 namespace App\Models\Eccube3;
 
 use App\Models\Eccube3\AbstractEccube3Model;
+use App\Models\Eccube3\DtbProductClass as E3_DtbProductClass;
 
 class DtbProduct extends AbstractEccube3Model
 {
@@ -11,7 +12,12 @@ class DtbProduct extends AbstractEccube3Model
     protected $primaryKey = 'product_id';
 
     protected $casts = [
-        //
+        'del_flg' => 'boolean',
     ];
+
+    public function productClasses()
+    {
+        return $this->hasMany(E3_DtbProductClass::class, 'product_id', 'product_id');
+    }
 
 }
